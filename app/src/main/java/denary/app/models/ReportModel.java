f8e81a62be344bbf9923986eb2405845f71276cb
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2014-04-05 21:20:55 -0400

    splash screen added

--- app/src/main/java/denary/app/models/ReportModel.java
+++ app/src/main/java/denary/app/models/ReportModel.java
@@ -3,5 +3,9 @@ package denary.app.models;
 /**
  * Created by gtkesh on 3/30/14.
  */
-public class ReportModel {
+public class ReportModel implements RModel{
+    @Override
+    public void generateReport(User user) {
+
+    }
 }
