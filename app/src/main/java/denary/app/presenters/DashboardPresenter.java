3aa254b76478518c11bfb76dd4779827a4824302
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2014-04-07 02:32:41 -0400

    Dashboard account list added

--- app/src/main/java/denary/app/presenters/DashboardPresenter.java
+++ app/src/main/java/denary/app/presenters/DashboardPresenter.java
@@ -3,23 +3,30 @@ package denary.app.presenters;
 import denary.app.models.Account;
 import denary.app.models.AccountModel;
 import denary.app.models.User;
+import denary.app.views.IDashboardView;
 import denary.app.views.IView;
 
 /**
  * Created by gtkesh on 4/7/14.
  */
 public class DashboardPresenter {
-    private final IView myView;
+    private final IDashboardView myView;
 
     private final AccountModel myModel;
 
-    public DashboardPresenter(IView view, AccountModel model) {
+    public DashboardPresenter(IDashboardView view, AccountModel model) {
         myView = view;
         myModel = model;
     }
 
     public void advance(){
         myView.advance();
+    }
+    public void advance(String s){
+        myView.advance(s);
+    }
+    public void advanceAlternative(){
+        myView.advanceAlternative();
     }
 
 }
---
+++ app/src/main/java/denary/app/views/IDashboardView.java
@@ -0,0 +1,11 @@
+package denary.app.views;
+
+/**
+ * Created by gtkesh on 4/7/14.
+ */
+public interface IDashboardView {
+    void advance();
+    void advance(String s);
+    void advanceAlternative();
+
+}
