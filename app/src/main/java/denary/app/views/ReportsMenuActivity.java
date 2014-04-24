4be1b277d7b006aa3ced610f77824873d8ac8bb6
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2014-04-24 01:02:48 -0400

    text recognition | reading reports in several languages

--- app/src/main/java/denary/app/views/ReportsMenuActivity.java
+++ app/src/main/java/denary/app/views/ReportsMenuActivity.java
@@ -5,8 +5,12 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.SearchManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,9 +19,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -26,7 +32,9 @@ import com.parse.SaveCallback;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import denary.app.R;
@@ -41,9 +49,12 @@ public class ReportsMenuActivity extends Activity implements  IView,TextToSpeech
     private static ReportPresenter myPresenter;
 
     private static Button generate_report_button;
+    private static Button translate_button;
     private TextView reportView;
     private String report = "";
     private TextToSpeech tts;
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,9 +105,28 @@ public class ReportsMenuActivity extends Activity implements  IView,TextToSpeech
                 speakOut(report);
             }
          });
-
+        translate_button = (Button) findViewById(R.id.translate_button);
+        translate_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak(v);
+            }
+        });
+        checkVoiceRecognition();
     }
 
+    public void checkVoiceRecognition() {
+        // Check if voice recognition is present
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
+                RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        if (activities.size() == 0) {
+            translate_button.setEnabled(false);
+            translate_button.setText("Voice recognizer not present");
+            Toast.makeText(this, "Voice recognizer not present",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public void advance() {
@@ -143,5 +173,72 @@ public class ReportsMenuActivity extends Activity implements  IView,TextToSpeech
     private void speakOut(String text) {
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
+
+
+
+
+
+
+    public void speak(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        //Start the Voice recognizer activity for the result.
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE)
+
+            //If Voice recognition is successful then it returns RESULT_OK
+            if(resultCode == RESULT_OK) {
+
+                ArrayList<String> textMatchList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if (!textMatchList.isEmpty()) {
+                    String t = textMatchList.get(0).toLowerCase();
+                    if (t.contains("italian")) {
+                       tts.setLanguage(Locale.ITALIAN);
+                    }else if(t.contains("french")){
+                        tts.setLanguage(Locale.FRENCH);
+                    }else if(t.contains("german")){
+                        tts.setLanguage(Locale.GERMAN);
+                    }else if(t.contains("japanese")){
+                        tts.setLanguage(Locale.JAPANESE);
+                    }else if(t.contains("korean")){
+                        tts.setLanguage(Locale.KOREAN);
+                    }
+                    else {
+                        System.out.println("NOT RECOGNIZED: " + textMatchList.toString());
+                        tts.setLanguage(Locale.ENGLISH);
+                        String s = "Language Not Supported";
+                        showToastMessage(s);
+                        speakOut(s);
+                    }
+
+                }
+                //Result code for various error.
+            }else if(resultCode == RecognizerIntent.RESULT_AUDIO_ERROR){
+                showToastMessage("Audio Error");
+            }else if(resultCode == RecognizerIntent.RESULT_CLIENT_ERROR){
+                showToastMessage("Client Error");
+            }else if(resultCode == RecognizerIntent.RESULT_NETWORK_ERROR){
+                showToastMessage("Network Error");
+            }else if(resultCode == RecognizerIntent.RESULT_NO_MATCH){
+                showToastMessage("No Match");
+            }else if(resultCode == RecognizerIntent.RESULT_SERVER_ERROR){
+                showToastMessage("Server Error");
+            }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    /**
+     * Helper method to show the toast message
+     **/
+    void showToastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 }
+
 
