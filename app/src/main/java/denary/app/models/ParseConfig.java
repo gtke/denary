5149937eeb45f89cd9333bde141ac303b7bb6be3
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2014-07-25 10:18:21 -0700

    Update ParseConfig.java

--- app/src/main/java/denary/app/models/ParseConfig.java
+++ app/src/main/java/denary/app/models/ParseConfig.java
@@ -20,8 +20,8 @@ public class ParseConfig {
     }
 
     private void parseInit(Context context) throws ParseException {
-        Parse.initialize(context,"y6YnnaGukRlmugs9KphSLBH3i1SnJfJw3qWvn6GH",
-                "L19EYqDDXZ9bBMyM0VQkVCErn8vDDBQJr4CJiqo6");
+        Parse.initialize(context,"XXX",
+                "XXX");
     }
 
 }
